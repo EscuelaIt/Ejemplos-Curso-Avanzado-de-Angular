d3624db441a39a46ba6566d6081c467b0ab8db42
Alberto Morales <kodenix@gmail.com>
2023-12-20 18:37:02 +0100

    Finalized signal

--- src/app/change-detection/example-signal/example-signal.component.ts
+++ src/app/change-detection/example-signal/example-signal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Signal, WritableSignal, computed, signal } from '@angular/core';
+import { Component, EffectRef, Injector, Signal, WritableSignal, computed, effect, signal, untracked } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PERSONS, PERSONS2 } from '../data';
 import { fibonacciPoorPerformance } from '../fibonacci';
@@ -22,6 +22,14 @@ import { Person } from '../Person';
       
       <p>Value is {{ conditionalCount() }}</p>
     </div>
+    <div>
+      <button (click)="startEffect()">Start effect</button>
+      <button (click)="stopEffect()">Stop effect</button>
+      <button (click)="setNewList()">Asignar nueva lista</button>
+    </div>
+    <div>
+      {{ listCalculated() }}
+    </div>
   </div>`,
   styles: ``,
 })
@@ -34,6 +42,51 @@ export class ExampleSignalComponent {
   mutableVisibilityValue: WritableSignal<boolean> = signal(false);
   readOnlyNumberValue: Signal<number> = this.mutableCounterValue.asReadonly();
   expensiveDataCalculated: WritableSignal<number[]> = signal(this.loadCalculatedPersonData(this.personsWithExpensiveValues))
+  myEffect!: EffectRef;  
+
+  myList = signal(['madrid']);
+
+  listCalculated = computed(()=> {
+    const value = this.myList().length;
+    return `date: ${new Date().toTimeString()} + value: ${value}`;
+  })
+  
+  constructor(private injector: Injector) {}
+
+  setNewList() {
+    this.myList.set(['madrid']);
+  }
+
+  startEffect() {
+    this.myEffect = effect((onCleanup)=> {
+      console.log('Launched effect');
+      //const numberValue = this.readOnlyNumberValue()
+      const visibility = this.mutableVisibilityValue();
+      const value2 = untracked(() => this.auxCalcul());
+      const timer = setTimeout(() => {
+        //console.log(`El valor de count es: ${numberValue}`)
+        console.log(`El valor de count es: ${value2} y la visibilidad es ${visibility}`)
+      }, 2000);
+
+      // Se ejecuta antes de lanzarse el effect y cuando se destruye el effect
+      onCleanup(() => {
+        clearTimeout(timer);
+        // this.logEffectClean();
+      }) 
+    }, { injector: this.injector });
+  }
+
+  auxCalcul(): number {
+    return 4 + this.readOnlyNumberValue();
+  }
+
+  logEffectClean() {
+    console.log('Me he limpiado');
+  }
+
+  stopEffect() {
+    this.myEffect.destroy();
+  }
 
   loadCalculatedPersonData(persons: Person[]): number[]{
     console.log('running loadCalculatedPersonData');
