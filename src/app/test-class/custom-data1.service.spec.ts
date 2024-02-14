10f85eaa787ae4b7c81c2e518bb075d26ad4b2e9
Alberto Morales <kodenix@gmail.com>
2024-02-14 17:14:33 +0100

    avance

--- src/app/test-class/custom-data1.service.spec.ts
+++ src/app/test-class/custom-data1.service.spec.ts
@@ -2,17 +2,26 @@ import { TestBed } from '@angular/core/testing';
 
 import { CustomData1Service } from './custom-data1.service';
 
+function sumar(val1: number, val2: number): number {
+  return val1 + val2
+}
+
 describe('CustomData1Service', () => {
-  let service: CustomData1Service;
+  let myService: CustomData1Service;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CustomData1Service);
+    TestBed.configureTestingModule({
+      providers: [CustomData1Service]
+    });
+    myService = TestBed.inject(CustomData1Service);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(myService).toBeTruthy();
   });
-  
+
+  it('should return true', () => {
+    expect(myService.getData1()).toBe(true);
+  });
 
 });
--- src/app/test-class/custom-data1.service.ts
+++ src/app/test-class/custom-data1.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class CustomData1Service {
 
   constructor() { }
+
+  getData1(): boolean {
+    return true;
+  }
 }
